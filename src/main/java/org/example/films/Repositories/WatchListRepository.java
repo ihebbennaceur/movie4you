4b262db362450ae980895ad12b237c9d7369c22e
ihebbennaceur <ihebbennaceur6@gmail.com>
2024-11-30 19:35:21 +0100

    remove watchlist

--- src/main/java/org/example/films/Repositories/WatchListRepository.java
+++
@@ -1,9 +0,0 @@
-//package org.example.films.Repositories;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface WatchListRepository extends JpaRepository<WatchList, Integer> {
-//   void deleteByUserIdAndMovieId(int userId, int movieId);
-//}
